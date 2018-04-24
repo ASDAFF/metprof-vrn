368c373e458e563352bc2f1e56f43cb554c1c8aa
sv@prime-ltd.su <metprof-vrn@server.metprof-vrn.ru>
2018-04-24 15:08:02 +0300

    srv

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("«Полимер» — главная страница сайта");
+$APPLICATION->SetTitle("«Металлинвест Профиль» — главная страница сайта");
 ?>
 <div class="mp__categories">
 
