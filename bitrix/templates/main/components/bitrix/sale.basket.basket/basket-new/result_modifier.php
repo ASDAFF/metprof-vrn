31eb248efd43089b3264fbc2b99d519d9b4a7b53
neeil1990 <neeil@mail.ru>
2018-05-03 12:59:43 +0300

    no message

--- bitrix/templates/main/components/bitrix/sale.basket.basket/basket-new/result_modifier.php
+++ bitrix/templates/main/components/bitrix/sale.basket.basket/basket-new/result_modifier.php
@@ -7,3 +7,19 @@ $arUrls = array(
     "add" => $curPage."add&id=#ID#",
 );
 unset($curPage);
+
+foreach($arResult["GRID"]["ROWS"] as &$row){
+
+    $mxResult = CCatalogSku::GetProductInfo($row['PRODUCT_ID']);
+    if (is_array($mxResult))
+    {
+        $arSelect = Array("ID", "NAME", "DETAIL_PAGE_URL");
+        $arFilter = Array("IBLOCK_ID" => $mxResult['IBLOCK_ID'], "ID" => $mxResult['ID'], "ACTIVE"=>"Y");
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+        if($arFields = $res->GetNext())
+        {
+            $row['DETAIL_PAGE_URL'] = $arFields['DETAIL_PAGE_URL'];
+        }
+    }
+
+}
